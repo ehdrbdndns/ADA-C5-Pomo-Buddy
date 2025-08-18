1e4beedb19837d6350b658671ccc958a124f10b9
ehdrbdndns <ehdrbdndns@naver.com>
2025-08-18 12:29:46 +0900

    feat: add autoTimer, language

--- ADA-C5-Pomo Buddy/Models/TimerSettings.swift
+++ ADA-C5-Pomo Buddy/Models/TimerSettings.swift
@@ -1,14 +1,27 @@
-
 import Foundation
 import SwiftData
+
+/// Enum to manage language settings in a type-safe way.
+enum LanguageSetting: String, Codable {
+    case systemDefault
+    case korean
+    case english
+}
 
 @Model
 final class TimerSettings {
     var focusDuration: TimeInterval
     var breakDuration: TimeInterval
+    var isAutoTimerEnabled: Bool
+    var language: LanguageSetting
 
-    init(focusDuration: TimeInterval = 25 * 60, breakDuration: TimeInterval = 5 * 60) {
+    init(focusDuration: TimeInterval = 25 * 60, 
+         breakDuration: TimeInterval = 5 * 60, 
+         isAutoTimerEnabled: Bool = false, 
+         language: LanguageSetting = .systemDefault) {
         self.focusDuration = focusDuration
         self.breakDuration = breakDuration
+        self.isAutoTimerEnabled = isAutoTimerEnabled
+        self.language = language
     }
 }
