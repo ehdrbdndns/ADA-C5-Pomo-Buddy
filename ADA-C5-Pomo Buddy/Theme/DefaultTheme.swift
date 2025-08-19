a0ce3e3d54897a62b90fc6b064629f30c6c3a29b
ehdrbdndns <ehdrbdndns@naver.com>
2025-08-19 13:31:31 +0900

    feat: AppTheme 수정

--- ADA-C5-Pomo Buddy/Theme/DefaultTheme.swift
+++ ADA-C5-Pomo Buddy/Theme/DefaultTheme.swift
@@ -7,44 +7,48 @@ struct DefaultTheme: AppThemeProfile {
     
     // MARK: Light Mode Themes
     private static let idleLight = AppTheme(
+        coin: AppColor.amber800,
         workingType: AppColor.gray500,
         timer: AppColor.gray700,
         primaryAction: AppColor.yellow400,
         secondaryAction: nil,
-        explan: AppColor.yellow700,
+        explain: AppColor.yellow700,
         settingState: AppColor.gray500,
         background: AppColor.yellow50,
         primaryButtonText: AppColor.yellow900
     )
 
     private static let focusingLight = AppTheme(
+        coin: AppColor.amber800,
         workingType: AppColor.gray500,
         timer: AppColor.gray700,
         primaryAction: AppColor.yellow400,
         secondaryAction: AppColor.red400,
-        explan: AppColor.amber700,
+        explain: AppColor.amber700,
         settingState: AppColor.gray500,
         background: AppColor.amber50,
         primaryButtonText: AppColor.yellow900
     )
 
     private static let breakingLight = AppTheme(
+        coin: AppColor.amber800,
         workingType: AppColor.gray500,
         timer: AppColor.gray700,
         primaryAction: AppColor.lime300,
         secondaryAction: nil,
-        explan: AppColor.lime700,
+        explain: AppColor.lime700,
         settingState: AppColor.gray500,
         background: AppColor.lime50,
         primaryButtonText: AppColor.lime800
     )
 
     private static let pausedLight = AppTheme(
+        coin: AppColor.amber800,
         workingType: AppColor.gray500,
         timer: AppColor.gray700,
         primaryAction: AppColor.yellow400,
         secondaryAction: AppColor.red400,
-        explan: AppColor.gray700,
+        explain: AppColor.gray700,
         settingState: AppColor.gray500,
         background: AppColor.gray50,
         primaryButtonText: AppColor.yellow900
@@ -52,44 +56,48 @@ struct DefaultTheme: AppThemeProfile {
 
     // MARK: Dark Mode Themes
     private static let idleDark = AppTheme(
+        coin: AppColor.amber300,
         workingType: AppColor.neutral400,
         timer: AppColor.zinc50,
         primaryAction: AppColor.yellow600,
         secondaryAction: nil,
-        explan: AppColor.yellow300,
+        explain: AppColor.yellow300,
         settingState: AppColor.neutral400,
         background: AppColor.DarkModeCustom.backgroundIdle,
         primaryButtonText: AppColor.yellow100
     )
 
     private static let focusingDark = AppTheme(
+        coin: AppColor.amber300,
         workingType: AppColor.neutral400,
         timer: AppColor.zinc50,
         primaryAction: AppColor.amber600,
         secondaryAction: AppColor.red600,
-        explan: AppColor.amber300,
+        explain: AppColor.amber300,
         settingState: AppColor.neutral400,
         background: AppColor.DarkModeCustom.backgroundFocus,
         primaryButtonText: AppColor.amber100
     )
 
     private static let breakingDark = AppTheme(
+        coin: AppColor.amber300,
         workingType: AppColor.neutral400,
         timer: AppColor.zinc50,
         primaryAction: AppColor.lime600,
         secondaryAction: nil,
-        explan: AppColor.lime300,
+        explain: AppColor.lime300,
         settingState: AppColor.neutral400,
         background: AppColor.DarkModeCustom.backgroundBreak,
         primaryButtonText: AppColor.lime100
     )
 
     private static let pausedDark = AppTheme(
+        coin: AppColor.amber300,
         workingType: AppColor.neutral400,
         timer: AppColor.zinc50,
         primaryAction: AppColor.amber600,
         secondaryAction: AppColor.red600,
-        explan: AppColor.yellow300,
+        explain: AppColor.yellow300,
         settingState: AppColor.neutral400,
         background: AppColor.DarkModeCustom.backgroundIdle,
         primaryButtonText: AppColor.amber100
