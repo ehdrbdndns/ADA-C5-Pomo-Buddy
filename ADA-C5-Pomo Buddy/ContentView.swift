e892b77d024d89694adba179ded996b5fdf3782c
ehdrbdndns <ehdrbdndns@naver.com>
2025-08-19 13:33:39 +0900

    feat: TimerView 코드 병합

--- ADA-C5-Pomo Buddy/ContentView.swift
+++ ADA-C5-Pomo Buddy/ContentView.swift
@@ -1,21 +1,28 @@
 import SwiftUI
 
+enum PageIndex: Int {
+    case history = 0
+    case timer = 1
+    case settings = 2
+}
+
 struct ContentView: View {
     // 0: Settings, 1: Timer, 2: History
-    @State private var selection: Int = 1
+    @State private var selection: PageIndex = .timer
 
     var body: some View {
         TabView(selection: $selection) {
-            SettingsView()
-                .tag(0)
-
-            TimerView()
-                .tag(1)
-
             HistoryView()
-                .tag(2)
+                .tag(PageIndex.history)
+            
+            TimerView()
+                .tag(PageIndex.timer)
+            
+            SettingsView()
+                .tag(PageIndex.settings)
         }
         .tabViewStyle(.page(indexDisplayMode: .never)) // Creates the carousel/pager effect
+        .font(.custom("Quicksand", size: AppFont.textBase))
         .ignoresSafeArea()
     }
 }
